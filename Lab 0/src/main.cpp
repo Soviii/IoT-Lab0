b0ac751498db0a832ff227f1c29edd82ae5a3662
Soviii <94957224+Soviii@users.noreply.github.com>
2023-02-16 00:18:08 -0800

    Update main.cpp

--- Lab 0/src/main.cpp
+++ Lab 0/src/main.cpp
@@ -10,13 +10,13 @@ const int photoresistor_pin = GPIO_NUM_39;
 int max_light;
 int min_light;
 float light_val;
-int rotation;
+int rotation; // holds rotation for servo
 
 // for LED
 int LED_outpin = GPIO_NUM_22;
 int LED_inpin = GPIO_NUM_38;
 
-
+/* changes state of LED based on current state */
 void BlinkLight(){
   if(digitalRead(LED_inpin) == HIGH){
     digitalWrite(LED_outpin, LOW);
@@ -25,6 +25,7 @@ void BlinkLight(){
   }
 }
 
+/* makes LED blink 5 times when called */
 void CalibrationSequence(){
   for(int i = 0; i < 10; i++){
     BlinkLight();
@@ -40,13 +41,13 @@ void setup(){
   pinMode(LED_inpin, INPUT);
 
   // For Photoresistor 
-  // finding max light
+  // blinks 5 times then checks for max light (max light will be intentional/forced)
   CalibrationSequence();
   max_light = analogRead(photoresistor_pin);
   Serial.print("Max light is: ");
   Serial.print(max_light);
   
-  // finding min light
+  // blinks 5 times then checks for min light (min light will be intentional/forced)
   CalibrationSequence();
   min_light = analogRead(photoresistor_pin);
   Serial.print("\nMin Light is ");
@@ -54,8 +55,9 @@ void setup(){
   Serial.print("\n");
 
   // For Servo
-	myservo.attach(servo_pin); // attaches the servo on port 32 to the servo object
+  myservo.attach(servo_pin); // attaches the servo on port 32 to the servo object
 
+  // turns off LED for main function in loop()
   digitalWrite(LED_outpin, LOW);
 }
 
@@ -64,12 +66,14 @@ void loop()
   //For Photoresistor
   light_val = analogRead(photoresistor_pin);
 
+  // find the percentage of current light to max light, multiply by 180 to find next angle
   rotation = light_val / max_light * 180;
 
   Serial.print("curr_rotation is: ");
   Serial.print(rotation);
   Serial.print("\n");
 
+  // output rotation to servo
   myservo.write(rotation);
 
   Serial.println(light_val);
